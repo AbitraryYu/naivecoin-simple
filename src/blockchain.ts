ab70de0ad5701cdbdd47f1716b532f4eaa1041a1
vincentli <vincent99v1123li@gmail.com>
2022-02-20 22:05:13 +0800

    blocks now stored and loaded from block.json file

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -48,7 +48,9 @@ const genesisBlock: Block = new Block(
     0, '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627', '', 1465154705, [genesisTransaction], 0, 0
 );
 
-let blockchain: Block[] = [genesisBlock];
+
+let blockchain: Block[] = BLOCKS_FILE;
+// let blockchain: Block[] = [genesisBlock];
 
 
 // the unspent txOut of genesis block is set to unspentTxOuts on startup
